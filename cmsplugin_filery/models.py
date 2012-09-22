d92269196131e9c7ef1c3e7def0df0b3a57b7618
Alireza Savand <alireza.savand@gmail.com>
2012-09-23 03:31:16 +0400

    Added Filery class
    The actual cms plugin
    Class that represents a photo gallery, galleries have the following
        properties:
            title: The gallery title, if given it will be displayed in the
                gallery

--- cmsplugin_filery/models.py
+++ cmsplugin_filery/models.py
@@ -6,14 +6,32 @@ from django.utils.translation import ugettext_lazy as _
 from filer.fields.image import FilerImageField
 
 
+class Filery(CMSPlugin):
+    """
+     Class that represents a photo gallery, galleries have the following
+     properties:
 
+    ``title``
+        The gallery title, if given it will be displayed in the gallery
+        header (optional).
+    """
 
+    title = models.CharField(
+        _('title'),
+        max_length=50,
+        blank=True,
+        help_text=_('The gallery title, if given it will be displayed in '\
+                    'the gallery header (optional)')
+    )
 
     def __unicode__(self):
         return _(u'%(count)d image(s) in gallery') % {'count': self.image_set.count()}
 
 
 
+    class Meta:
+        verbose_name = _('filery')
+        verbose_name_plural = _('fileries')
 
 
 
